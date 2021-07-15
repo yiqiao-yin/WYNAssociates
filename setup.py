0c6a86c4247555d59ada3626d65e96fd9a919e16
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2021-07-14 22:35:53 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name='YinPortfolioManagement',
+  author='Yiqiao Yin',
+  version='1.0.0',
+  description="This package uses Long Short-Term Memory (LSTM) to forecast a stock price that user enters.",
+  packages=['forecast']
+)
