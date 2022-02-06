42022078dc8f00fdf250f19d80f34a1cd685cda6
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2022-02-06 17:09:38 -0500

    Update modules.py

--- AI_solution/modules.py
+++ AI_solution/modules.py
@@ -1514,6 +1514,63 @@ class YinsML:
     """
 
     # Define function
+    def LogisticRegression_Classifier(X_train, X_test, y_train, y_test, random_state = 0):
+        
+        # Import Modules
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import pandas as pd
+        import random
+        from sklearn.linear_model import LogisticRegression
+        
+        # Train
+        LOGIT_Clf = LogisticRegression( random_state=random_state )
+        LOGIT_Clf = LOGIT_Clf.fit(X_train, y_train)
+        
+        # Report In-sample Estimators
+        y_train_hat_ = LOGIT_Clf.predict(X_train)
+        y_train_hat_score = LOGIT_Clf.predict_proba(X_train)
+
+        from sklearn.metrics import confusion_matrix
+        confusion_train = pd.DataFrame(confusion_matrix(y_train_hat_, y_train))
+        confusion_train
+        
+        train_acc = sum(np.diag(confusion_train)) / sum(sum(np.array(confusion_train)))
+        train_acc
+
+        y_test_hat_ = LOGIT_Clf.predict(X_test)
+        y_test_hat_score = LOGIT_Clf.predict_proba(X_test)
+        confusion_test = pd.DataFrame(confusion_matrix(y_test_hat_, y_test))
+        confusion_test
+
+        test_acc = sum(np.diag(confusion_test)) / sum(sum(np.array(confusion_test)))
+        test_acc
+        
+        # Output
+        return {
+            'Data': {
+                'X_train': X_train, 
+                'y_train': y_train, 
+                'X_test': X_test, 
+                'y_test': y_test
+            },
+            'Model': LOGIT_Clf,
+            'Train Result': {
+                'y_train_hat_': y_train_hat_,
+                'y_train_hat_score': y_train_hat_score,
+                'confusion_train': confusion_train,
+                'train_acc': train_acc
+            },
+            'Test Result': {
+                'y_test_hat_': y_test_hat_,
+                'y_test_hat_score': y_test_hat_score,
+                'confusion_test': confusion_test,
+                'test_acc': test_acc
+            }
+        }
+    # End of function
+
+    # Define function
     def DecisionTree_Classifier(X_train, X_test, y_train, y_test, maxdepth = 3):
         
         # Import Modules
@@ -1730,64 +1787,7 @@ class YinsML:
             }
         }
     # End of function
-    
-    # Define function
-    def Logistic_Classifier(X_train, X_test, y_train, y_test, random_state = 0):
         
-        # Import Modules
-        import numpy as np
-        import matplotlib.pyplot as plt
-        import pandas as pd
-        import random
-        from sklearn.linear_model import LogisticRegression
-        
-        # Train
-        LOGIT_Clf = LogisticRegression( random_state=random_state )
-        LOGIT_Clf = LOGIT_Clf.fit(X_train, y_train)
-        
-        # Report In-sample Estimators
-        y_train_hat_ = LOGIT_Clf.predict(X_train)
-        y_train_hat_score = LOGIT_Clf.predict_proba(X_train)
-
-        from sklearn.metrics import confusion_matrix
-        confusion_train = pd.DataFrame(confusion_matrix(y_train_hat_, y_train))
-        confusion_train
-        
-        train_acc = sum(np.diag(confusion_train)) / sum(sum(np.array(confusion_train)))
-        train_acc
-
-        y_test_hat_ = LOGIT_Clf.predict(X_test)
-        y_test_hat_score = LOGIT_Clf.predict_proba(X_test)
-        confusion_test = pd.DataFrame(confusion_matrix(y_test_hat_, y_test))
-        confusion_test
-
-        test_acc = sum(np.diag(confusion_test)) / sum(sum(np.array(confusion_test)))
-        test_acc
-        
-        # Output
-        return {
-            'Data': {
-                'X_train': X_train, 
-                'y_train': y_train, 
-                'X_test': X_test, 
-                'y_test': y_test
-            },
-            'Model': LOGIT_Clf,
-            'Train Result': {
-                'y_train_hat_': y_train_hat_,
-                'y_train_hat_score': y_train_hat_score,
-                'confusion_train': confusion_train,
-                'train_acc': train_acc
-            },
-            'Test Result': {
-                'y_test_hat_': y_test_hat_,
-                'y_test_hat_score': y_test_hat_score,
-                'confusion_test': confusion_test,
-                'test_acc': test_acc
-            }
-        }
-    # End of function
-    
     # Define function
     def Adam_Regressor(Xadam, y, batch_size = 10, lr = 0.01, epochs = 200, period = 20, verbose=True):
         
