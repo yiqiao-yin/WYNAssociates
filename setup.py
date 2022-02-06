52bfccea9541cc5d8266d4f30bc839b27ff573ea
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2022-02-06 01:54:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(
   version='1.0.0',
   description="This package provides AI-driven solutions.",
   packages=find_packages()
+  packages=find_packages(include=["AI-solutions", "YinCapital_forecast"]),
 )
