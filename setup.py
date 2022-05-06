3c4b5fd4515bcc847db1672371bbd3f56ef5bdf9
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2022-05-06 11:11:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name='WYNAssociates',
   author='Yiqiao Yin',
   author_email="Yiqiao.Yin@wyn-associates.com",
-  version='1.0.0',
+  version='1.1',
   license="MIT license",
   description="This package provides AI-driven solutions.",
   packages=["AI_solution", "FIN_solution", "ML_solution"]
