1cf646f8c73ad86d3cebc13957fa6e9642b49c31
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2022-06-15 11:57:48 -0400

    Update modules.py

--- AI_solution/modules.py
+++ AI_solution/modules.py
@@ -851,6 +851,50 @@ class YinsDL:
         }
     # End of function
     
+    # transfer learning: using VGG16
+    # add layer
+    def from_vgg16(input_shape, n_classes, optimizer='rmsprop', fine_tune=0):
+        """
+        Compiles a model integrated with VGG16 pretrained layers
+
+        input_shape: tuple  - the shape of input images (width, height, channels)
+        n_classes:   int    - number of classes for the output layer
+        optimizer:   string - instantiated optimizer to use for training. Defaults to 'RMSProp'
+        fine_tune:   int    - The number of pre-trained layers to unfreeze.
+                              If set to 0, all pretrained layers will freeze during training
+        """
+
+        # Pretrained convolutional layers are loaded using the Imagenet weights.
+        # Include_top is set to False, in order to exclude the model's fully-connected layers.
+        conv_base = tf.keras.applications.vgg16.VGG16(include_top=False,
+                         weights='imagenet', 
+                         input_shape=input_shape)
+
+        # Defines how many layers to freeze during training.
+        # Layers in the convolutional base are switched from trainable to non-trainable
+        # depending on the size of the fine-tuning parameter.
+        if fine_tune > 0:
+            for layer in conv_base.layers[:-fine_tune]:
+                layer.trainable = False
+        else:
+            for layer in conv_base.layers:
+                layer.trainable = False
+
+        # Create a new 'top' of the model (i.e. fully-connected layers).
+        # This is 'bootstrapping' a new top_model onto the pretrained layers.
+        top_model = conv_base.output
+        top_model = tf.keras.layers.Flatten(name="flatten")(top_model)
+        top_model = tf.keras.layers.Dense(4096, activation='relu')(top_model)
+        top_model = tf.keras.layers.Dense(1072, activation='relu')(top_model)
+        top_model = tf.keras.layers.Dropout(0.2)(top_model)
+        output_layer = tf.keras.layers.Dense(n_classes, activation='softmax')(top_model)
+
+        # Group the convolutional base and new fully-connected layers into a Model object.
+        model = tf.keras.Model(inputs=conv_base.input, outputs=output_layer)
+
+        # output
+        return model
+    
     # define NeuralNet_Regressor function:
     def NeuralNet_Regressor(
             X_train=None,
