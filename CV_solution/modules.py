91fc25f04f0adbb7cebd8f7d0e3b274c7f3192d9
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2022-06-14 23:28:10 -0400

    Update modules.py

--- CV_solution/modules.py
+++ CV_solution/modules.py
@@ -109,6 +109,68 @@ class YinsCV:
 
         return model
     
+    # define unet
+    def inception_unet_model(img_size=(128, 128), num_classes=2, ENC_PARAM=[64, 128, 256]):
+        inputs = keras.Input(shape=img_size)
+
+        ### [First half of the network: downsampling inputs] ###
+        ENC_PARAM = ENC_PARAM
+
+        # Entry block
+        x = layers.Conv2D(ENC_PARAM[0], 3, strides=2, padding="same")(inputs)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+
+        previous_block_activation = x  # Set aside residual
+
+        # Blocks 1, 2, 3 are identical apart from the feature depth.
+        for filters in ENC_PARAM[1::]:
+            x = layers.Activation("relu")(x)
+            x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.Activation("relu")(x)
+            x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+            # Project residual
+            residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+                previous_block_activation
+            )
+            x = layers.add([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        ### [Second half of the network: upsampling inputs] ###
+        DEC_PARAM = ENC_PARAM[::-1]
+
+        for filters in DEC_PARAM:
+            x = layers.Activation("relu")(x)
+            x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.Activation("relu")(x)
+            x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.UpSampling2D(2)(x)
+
+            # Project residual
+            residual = layers.UpSampling2D(2)(previous_block_activation)
+            residual = layers.Conv2D(filters, 1, padding="same")(residual)
+            x = layers.add([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        # Add a per-pixel classification layer
+        outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+
+        # Define the model
+        model = keras.Model(inputs, outputs)
+
+        # return
+        return model    
+    
     # define unet (inception style)
     def unet_inceptionstyle_model(
         x_train=None,
