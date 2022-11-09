44a94f99ea9a9dba3ff89802acf31c9ea9e83b2a
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2022-11-09 01:40:06 -0500

    Create modules.py

---
+++ NLPsolution/modules.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from transformers import BertTokenizer, TFBertModel
+
+
+def get_models_():
+    """get the model for bert tokenizer and the model"""
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = TFBertModel.from_pretrained("bert-base-uncased")
+    return tokenizer, model
+
+
+def bert_encoder_(text: str):
+    """bert encoder: a unit function to embed one string"""
+    encoded_input = tokenizer(text, return_tensors='tf')
+    output = model(encoded_input)
+    return np.asarray(output[0]).sum()
+
+
+tokenizer, model = get_models_()
+
+
+def sort_names_(names: np.ndarray):
+    """embed an array of names through the bert space"""
+    results_ = []
+    for i in tqdm(range(names.shape[0])):
+        results_.append([
+            names[i, 0],
+            names[i, 1],
+            bert_encoder_(names[i, 0]),
+            bert_encoder_(names[i, 1]),
+            bert_encoder_(names[i, 2])
+        ])
+
+    results_ = np.asarray(results_)
+    results_ = pd.DataFrame(
+        results_[np.argsort(results_[:, 3]), :]
+    ).iloc[:, 0:2]
+
+    return results_
