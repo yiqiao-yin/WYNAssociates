0d77f1e69c824b0f42ad89372dfbcffa49da799f
Yiqiao Yin <eagle0504@gmail.com>
2022-12-19 17:11:27 -0500

    checkpt erased

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-  name='YinPortfolioManagement',
-  author='Yiqiao Yin',
-  version='1.0.0',
-  description="This package uses Long Short-Term Memory (LSTM) to forecast a stock price that user enters.",
-  packages=['YinCapital_forecast']
-)
