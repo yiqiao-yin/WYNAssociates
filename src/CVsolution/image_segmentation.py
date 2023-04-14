6770783fa77ec4e6ff34f882e8071d6691b4b2db
Yiqiao Yin <eagle0504@gmail.com>
2023-04-14 00:00:49 -0400

    code updated

--- src/CVsolution/image_segmentation.py
+++ src/CVsolution/image_segmentation.py
@@ -596,3 +596,51 @@ def unetpp(filters, output_channels, width=None, height=None, input_channels=1, 
     model = Model(inputs=inputs, outputs=outputs)
 
     return model
+
+
+# define unet
+def conv_block(input, num_filters):
+    x = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(input)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+    
+    x = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+    return x
+
+def encoder_block(input, num_filters):
+    x = conv_block(input, num_filters)
+    p = tf.keras.layers.MaxPool2D((2, 2))(x)
+    return x, p
+
+def decoder_block(input, skip_features, num_filters):
+    x = tf.keras.layers.Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    x = tf.keras.layers.Concatenate()([x, skip_features])
+    x = conv_block(x, num_filters)
+    return x
+
+def build_unet(input_shape):
+    inputs = tf.keras.Input(input_shape)
+
+    s1, p1 = encoder_block(inputs, 32)
+    s2, p2 = encoder_block(p1, 64)
+    s3, p3 = encoder_block(p2, 128)
+    s4, p4 = encoder_block(p3, 256)
+    s5, p5 = encoder_block(p4, 512)
+
+    b0 = conv_block(p5, 512)
+    flt_layer = tf.keras.layers.Flatten()(b0)
+    aux_dense_layer1 = tf.keras.layers.Dense(128, 'relu')(flt_layer)
+    aux_dense_layer2 = tf.keras.layers.Dense(2, 'softmax')(aux_dense_layer1)
+
+    d0 = decoder_block(b0, s5, 512)
+    d1 = decoder_block(d0, s4, 256)
+    d2 = decoder_block(d1, s3, 128)
+    d3 = decoder_block(d2, s2, 64)
+    d4 = decoder_block(d3, s1, 32)
+
+    outputs = tf.keras.layers.Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
+
+    model = tf.keras.Model(inputs, [outputs, aux_dense_layer2], name="U-Net+Classifier")
+    return model
